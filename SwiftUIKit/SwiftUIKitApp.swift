f06e0c057473a11be9a1f97275e05cc6445f04ea
Maria Kharybina <maria.karybina@bbc.co.uk>
2023-06-15 17:07:19 +0100

    Initial Commit

---
+++ SwiftUIKit/SwiftUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKitApp.swift
+//  SwiftUIKit
+//
+//  Created by Maria Kharybina on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
